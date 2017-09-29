befc14908d2fc8c86511b74bea311285663f7509
Andres Buzzio <andresbuzzio@gmail.com>
2017-09-29 15:45:35 +0200

    Add files via upload

---
+++ js/gigpro.js
@@ -0,0 +1,16 @@
+// we initialise the owlCarousel
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+
+/* carousel on homepage auto rotates now
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:4000,
+    autoplayHoverPause:true
+});
+* /
