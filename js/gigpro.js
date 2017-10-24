cc1674007d3c5bf108e6749c5eab9e2af5fb653c
Andres Buzzio <andresbuzzio@gmail.com>
2017-10-24 14:12:19 +0200

    Add files via upload

--- js/gigpro.js
+++ js/gigpro.js
@@ -4,13 +4,91 @@ $(document).ready(function(){
   $(".owl-carousel").owlCarousel();
 });
 
-/* carousel on homepage auto rotates now
 
-var owl = $('.owl-carousel');
-owl.owlCarousel({
-    loop:true,
-    autoplay:true,
-    autoplayTimeout:4000,
-    autoplayHoverPause:true
+// collapsable items
+
+$(".expand-booked-text").click(function () {
+
+    $content = $(".accordion-booked");
+
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+        /* $header.text(function () {
+            //change text based on condition
+            return $content.is(":visible") ? "Collapse" : "Expand";
+        }); */
+    });
+
 });
-* /
+
+$(".expand-booked-text2").click(function () {
+
+    $content2 = $(".accordion-booked2");
+
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content2.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+        /* $header.text(function () {
+            //change text based on condition
+            return $content.is(":visible") ? "Collapse" : "Expand";
+        }); */
+    });
+
+});
+
+$(".expand-booked-text3").click(function () {
+
+    $content2 = $(".accordion-booked3");
+
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content2.slideToggle(500, function () {
+    });
+
+});
+
+$(".expand-booked-text4").click(function () {
+
+    $content2 = $(".accordion-booked4");
+
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content2.slideToggle(500, function () {
+    });
+
+});
+
+$(".header2").click(function () {
+
+    $header2 = $(this);
+    //getting the next element
+    $content2 = $("#accordion2");
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content2.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+        /* $header.text(function () {
+            //change text based on condition
+            return $content.is(":visible") ? "Collapse" : "Expand";
+        }); */
+    });
+
+});
+
+$(".expand-add-event").click(function () {
+
+    $header = $(this);
+    //getting the next element
+    $content = $(".expand-me");
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+        /* $header.text(function () {
+            //change text based on condition
+            return $content.is(":visible") ? "Collapse" : "Expand";
+        }); */
+    });
+
+});
