ea155ee20d2af3524e1d3c1d7ba25dca2fb6a711
Andres Buzzio <andresbuzzio@gmail.com>
2017-10-25 13:38:18 +0200

    Add files via upload

--- js/gigpro.js
+++ js/gigpro.js
@@ -92,3 +92,56 @@ $(".expand-add-event").click(function () {
     });
 
 });
+
+// search dropdown
+
+function mySearch() {
+    // Declare variables
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+
+    document.getElementById("myUL").style.display = "block";
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+// search dropdown by City
+
+function mySearchCity() {
+    // Declare variables
+    var input, filter, ul, li, a, e;
+    input = document.getElementById('myCityInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myULCity");
+    li = ul.getElementsByTagName('li');
+
+    document.getElementById("myULCity").style.display = "block";
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (e = 0; e < li.length; e++) {
+        a = li[e].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[e].style.display = "";
+        } else {
+            li[e].style.display = "none";
+        }
+    }
+}
+
+function cleanFields() {
+
+    document.getElementById("myULCity").style.display = "none";
+    document.getElementById("myUL").style.display = "none";
+
+}
